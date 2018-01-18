3d02e7da9fd3340ae208efc90d0211a16a16ea11
Marina <motroshina@yandex.ru>
2018-01-18 15:38:02 +0300

    Modified MainActivity Added new test method getNumSum().

--- app/src/main/java/com/example/mary/myfirstcalculator/MainActivity.java
+++ app/src/main/java/com/example/mary/myfirstcalculator/MainActivity.java
@@ -9,5 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+    }
+
+    private void getNumSum(){
+        int numOne = 1;
+        int numTwo = 2;
+        int numResult = numOne + numTwo;
     }
 }
